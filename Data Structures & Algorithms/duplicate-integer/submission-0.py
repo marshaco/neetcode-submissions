42fcf23519ff1b268064495ce5d6786869e80223
Colin Marshall <marshaco@tcd.ie>
2026-06-18 11:55:49 +0100

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        hs = set()
+
+        for i in nums:
+            if i in hs:
+                return True
+            hs.add(i)
+
+        return False
+        
