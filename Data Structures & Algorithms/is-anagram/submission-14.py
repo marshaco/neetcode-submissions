0f19b8a8d1bd120fb3ccc7891ba0348f6b2c4c55
Colin Marshall <marshaco@tcd.ie>
2026-06-20 12:04:40 +0100

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        freqS = [0] * 26
+        freqT = [0] * 26
+
+        for i in s:
+            freqS[ord(i) - ord("a")] += 1
+
+        for i in t:
+            freqT[ord(i) - ord("a")] += 1
+
+        for i in range(len(freqS)):
+            if freqS[i] != freqT[i]:
+                return False
+
+        return True
