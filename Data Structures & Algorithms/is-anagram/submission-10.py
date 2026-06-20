e03193b1d6f7d1a75843e1e2d06886f57d959af7
Colin Marshall <marshaco@tcd.ie>
2026-06-20 11:38:13 +0100

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        sList = list(s)
+        tList = list(t)
+
+        sList.sort()
+        tList.sort()
+
+        for i in range(len(sList)):
+            if sList[i] != tList[i]:
+                return False
+
+        return True
