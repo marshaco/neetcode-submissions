367609942a2dd3a03c20990410fa49db73c45779
Colin Marshall <marshaco@tcd.ie>
2026-07-13 18:58:17 +0100

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strip = ""
+
+        for i in s:
+            if i.isalnum():
+                strip += i
+
+        r = len(strip) - 1
+        l = 0
+
+        while l <= r:
+
+            while not strip[l].isalnum():
+                l +=1
+
+            while not strip[r].isalnum():
+                r -= 1
+
+            if strip[l].lower() != strip[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
